962c5db4ce1b55fde116ffaf42e403c29ef8b2c8
Ryan Phillips <ryanmphill@gmail.com>
2023-08-21 17:48:38 -0500

    Retrieved top selling agent and their total sales

---
+++ top_2009_agent.sql
@@ -0,0 +1,15 @@
+--Which sales agent made the most in sales in 2009?
+
+--HINT: Use the MAX function on a subquery.
+
+SELECT SalesRep, MAX(TotalSales) FROM (
+    SELECT 
+        E.FirstName || ' ' || E.LastName AS SalesRep,
+        SUM(I.Total) AS TotalSales
+    FROM Employee E
+    LEFT JOIN CUSTOMER C ON E.EmployeeId = C.SupportRepId
+    LEFT JOIN INVOICE I ON C.CustomerId = I.CustomerId
+    WHERE E.Title LIKE '%Sales%' AND Title LIKE '%Agent%'
+    GROUP BY SalesRep
+);
+
